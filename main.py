cf94fdb6723cb8f68647b00801fb2ed7db580533
28sanjayrp <mediasocial425@gmial.com>
2022-11-01 18:28:00 +0530

    pythone inheritance

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+#Inheritance
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#class1
+class class1:
+
+    def __init__(self,n,c,s):
+        self.name = n
+        self.color = c
+        self.speed = s
+
+    def mydef(self):
+        print("\n Car name :", self.name, "\n Car color :",self.color, "\n speed of car :",self.speed)
+
+#creating obj
+bmw = class1("Bmw","red",180)
+audi = class1("Audi","black",190)
+ferrari =class1("Ferrari","silver",200)
+
+#obj calling
+bmw.mydef()
+audi.mydef()
+ferrari.mydef()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#class2
+
+class class2(class1):
+    def getspeed(self):
+        print("\n corrent speed :",self.speed)
+
+obj1 = class2("ford","red",120)
+
+obj1.getspeed()                                #o/p : corrent speed : 120
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#class3
+class class3(class1):
+    def getcarnames(self):
+        print("\n car name : ", self.name)
+
+obj2 = class3("cord","blue",140)
+
+obj2.getcarnames()                                      #o/p :  car name :  cord
+
+#mix
+print("\n Inherating outputs as below")
+obj1.mydef()             #obj1 is child object and mydef is parent class's function.
+obj1.getspeed()
+
+obj2.mydef()             #obj2 is another child object and mydef is parent class's function.
+obj2.getcarnames()
+
